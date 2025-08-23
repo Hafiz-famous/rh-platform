8947e778b966b12727e67334b0ef7220886521a6
hafiz famous <hafizalassani1@gmail.com>
2025-08-23 13:42:44 +0000

    chore(vercel): config python + entrée WSGI

--- api/index.py
+++ api/index.py
@@ -1,2 +1,9 @@
-﻿from app import create_app
+﻿# api/index.py
+from app import create_app
+
+# Vercel cherche une variable WSGI nommée "app"
 app = create_app()
+
+# utile en local uniquement
+if __name__ == "__main__":
+    app.run()
